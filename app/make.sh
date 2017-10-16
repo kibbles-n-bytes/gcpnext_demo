404a50ce1f40f7038575ef85e5e39db286417ce0
Michael Kibbe <mkibbe@google.com>
2017-10-16 15:57:09 -0700

    move containers to own registry

--- app/make.sh
+++ app/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build . -t "gcr.io/kibbe-plori/app:latest"
+docker build . -t "gcr.io/sc-bookstore-demo/app:test"
 gcloud docker -a
-docker push "gcr.io/kibbe-plori/app:latest"
+docker push "gcr.io/sc-bookstore-demo/app:test"
