9426306a88c7f4ebcb8708e519867eac0325b9f6
Michael Kibbe <mkibbe@google.com>
2017-10-17 12:08:33 -0700

    modify cluster cleanup script

--- cleancluster.sh
+++ cleancluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kubectl delete servicebrokers,deployments,services,pods,serviceclasses,servicebindings,serviceinstances,secrets,configmaps --all
+kubectl delete servicebrokers,serviceclasses,serviceinstancecredentials,serviceinstances,secrets,configmaps --all
 helm list | grep -v catalog-0.0 | grep -v NAME | awk '{print $1}'  | xargs helm delete
