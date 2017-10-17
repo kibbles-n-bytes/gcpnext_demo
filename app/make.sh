d4245d0e836c045008b0b532ab13f361b2d27bf6
Michael Kibbe <mkibbe@google.com>
2017-10-17 14:19:05 -0700

    modify make.sh for app

--- app/make.sh
+++ app/make.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
+
 docker build . -t "gcr.io/sc-bookstore-demo/app:test"
-gcloud docker -a
-docker push "gcr.io/sc-bookstore-demo/app:test"
+gcloud docker -- push "gcr.io/sc-bookstore-demo/app:test"
