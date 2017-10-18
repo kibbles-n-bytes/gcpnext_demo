77fb19cb78d181d688d5806aaa13ba3ab01461e7
Michael Kibbe <mkibbe@google.com>
2017-10-18 16:10:31 -0700

    tweak service account-related files

--- scripts/setup-service-account-key.sh
+++ scripts/setup-service-account-key.sh
@@ -3,7 +3,10 @@
 set -eu
 
 echo "Using GCP project: ${GCP_PROJECT}"
-echo "Using service account: ${SERVICE_ACCOUNT_ID}"
+echo "Using service account: ${SERVICE_ACCOUNT_NAME}"
+
+SECRET_NAME="${SECRET_NAME:-"gcp-service-account-key"}"
+echo "Using secret name: ${SECRET_NAME}"
 
 NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
 DIR="/tmp/sc-bookstore-demo${NEW_UUID}"
@@ -11,10 +14,9 @@ DIR="/tmp/sc-bookstore-demo${NEW_UUID}"
 echo "Making directory ${DIR}"
 mkdir ${DIR}
 
-echo "Creating key to service account ${SERVICE_ACCOUNT_ID} in ${DIR}"
+echo "Creating key to service account ${SERVICE_ACCOUNT_NAME} in ${DIR}"
 gcloud iam service-accounts keys create ${DIR}/key.json \
-          --iam-account ${SERVICE_ACCOUNT_ID}@${GCP_PROJECT}.iam.gserviceaccount.com
+  --iam-account ${SERVICE_ACCOUNT_NAME}@${GCP_PROJECT}.iam.gserviceaccount.com
 
-SECRET_NAME="${SECRET_NAME:-"pubsub-key"}"
 echo "Creating secret ${SECRET_NAME}"
 kubectl create secret generic ${SECRET_NAME} --from-file=key.json="${DIR}/key.json"
